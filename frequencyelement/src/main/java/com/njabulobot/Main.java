c8b2403d93429a194b9d88707b9924d5899ccf50
Njabulo <nnshiwu@gmail.com>
2024-02-17 11:56:28 +0200

    Done with Frequency element project.

--- frequencyelement/src/main/java/com/njabulobot/Main.java
+++ frequencyelement/src/main/java/com/njabulobot/Main.java
@@ -2,6 +2,20 @@ package com.njabulobot;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //Display the frequency of each element present in array
+        System.out.println("----------------------------");
+        System.out.println("Element | Frequency");
+        System.out.println("----------------------------");
+        frequencyArray freq = new frequencyArray();
+        givenArray give = new givenArray();
+        int [] giveArray = give.numberArray();
+        int [] freArray = freq.freqqArray();
+        for(int i = 0;i < freArray.length; i++){
+            if(freArray[i] != freq.visited){
+                System.out.println(" "+giveArray[i]+" | "+freArray[i]);
+            }
+            
+        }
+        System.out.println("----------------------------");
     }
 }
--- frequencyelement/src/main/java/com/njabulobot/frequencyArray.java
+++ frequencyelement/src/main/java/com/njabulobot/frequencyArray.java
@@ -1,22 +1,30 @@
 package com.njabulobot;
 
 public class frequencyArray {
-   givenArray array = new givenArray();
-   int [] arr = array.numberArray();
-
-   //Array frequency will store frequencies of an element
-   int [] frequency = new int[arr.length];
 
    int visited = -1;
-   for(int i = 0;i < arr.length;++i){
-      int count = 1;
-      for(int j = i+1;j < arr.length;j++){
-         if(arr[i] == arr[j]){
-            count++;
-            //To avoid counting same element again
-            frequency[j] = visited;
+   
+   int[] freqqArray(){
+
+      givenArray array = new givenArray();
+      int [] arr = array.numberArray();
+
+      //Array frequency will store frequencies of an element
+      int [] frequency = new int[arr.length];
+
+      for(int i = 0;i < arr.length;++i){
+         int count = 1;
+         for(int j = i+1;j < arr.length;j++){
+            if(arr[i] == arr[j]){
+               count++;
+               //To avoid counting same element again
+               frequency[j] = visited;
+            }
+         }
+         if(frequency[i]!=visited){
+            frequency[i] = count;
          }
       }
+      return frequency;
    }
-   
 }
