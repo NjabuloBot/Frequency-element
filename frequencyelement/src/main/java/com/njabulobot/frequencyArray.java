0ff383f64049478e5e25717fb93f182cdebc9f6e
Njabulo <nnshiwu@gmail.com>
2024-02-17 11:41:45 +0200

    Added second for loop to add the of the elements in the frequency array,with a if condition whether the occurency of an element in the array.

--- frequencyelement/src/main/java/com/njabulobot/frequencyArray.java
+++ frequencyelement/src/main/java/com/njabulobot/frequencyArray.java
@@ -10,6 +10,13 @@ public class frequencyArray {
    int visited = -1;
    for(int i = 0;i < arr.length;++i){
       int count = 1;
+      for(int j = i+1;j < arr.length;j++){
+         if(arr[i] == arr[j]){
+            count++;
+            //To avoid counting same element again
+            frequency[j] = visited;
+         }
+      }
    }
    
 }
