8964ad732d8dfdbc278e2cdee6c028d6c4b301e7
Njabulo <nnshiwu@gmail.com>
2024-02-17 11:34:05 +0200

    Added a frequencyArray.java class,created an object array for givenArray class,used that object to access the given array and assigned it to arr,then created a frequency array that will be the same length as the given array.

--- frequencyelement/src/main/java/com/njabulobot/frequencyArray.java
+++ frequencyelement/src/main/java/com/njabulobot/frequencyArray.java
@@ -1,5 +1,11 @@
 package com.njabulobot;
 
 public class frequencyArray {
+   givenArray array = new givenArray();
+   int [] arr = array.numberArray();
+
+   //Array frequency will store frequencies of an element
+   int [] frequency = new int[arr.length];
+   
    
 }
